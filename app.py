a1ccf7482087bc6d18dfd45c17f0137cbac93c20
takahashi-koken <138287350+takahashi-koken@users.noreply.github.com>
2023-11-17 15:39:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # 以下を「app.py」に書き込み
 import streamlit as st
 import openai
+pip install --upgrade openai 
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
